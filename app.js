39efb7cd5dbd6960529c15392753a4cf66cf6853
Iquan <z98428222@gmail.com>
2024-04-08 21:42:29 +0800

    Async and Await

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const e = require("express");
-
-const fetchPromise1 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
-);
-const fetchPromise2 = fetch(
-  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
-);
-const fetchPromise3 = fetch(
-  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
-);
-
-Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
-  .then((responses) => {
-    responses.forEach((response) => {
-      console.log(response.url, response.status);
-    });
-  })
-  .catch((e) => {
+async function fetchProduct() {
+  try {
+    const response = await fetch(
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  } catch (e) {
     console.log(e);
-  });
+  }
+}
+
+fetchProduct();
+
+async function my() {
+  return 10;
+}
+let result = my();
+result.then((data) => {
+  console.log(data);
+});
